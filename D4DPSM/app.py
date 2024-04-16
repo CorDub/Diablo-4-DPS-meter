ec391a109a8b0c95fdf043ad1b90742b2ad43a61
Corentin Dubois <corentindubois22@gmail.com>
2024-04-16 15:52:49 -0600

    Making this a package for ease of use

--- D4DPSM/app.py
+++ D4DPSM/app.py
@@ -1,9 +1,5 @@
 import gradio as gr
-import easyocr
-from processing import process_video_and_display_results
-
-# Start model
-reader = easyocr.Reader(['en'], gpu=True)
+from D4DPSM.processing import process_video_and_display_results
 
 # Gradio interface code with custom CSS
 iface = gr.Interface(theme=gr.themes.Monochrome(), fn=process_video_and_display_results, inputs="video", outputs=["text", "image"],
